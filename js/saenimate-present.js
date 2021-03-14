bdcc61bd136226acd5410928bc527f65cd0739df
bill noto <williamnonato9@gmail.com>
2021-03-14 20:13:55 +0100

    workingPrototype

--- js/saenimate-present.js
+++ js/saenimate-present.js
@@ -1,21 +1,51 @@
-document.getElementById('blastIn').onclick = function () {
+document.getElementById('spin').onclick = function () {
     clearPreviousAnimation();
-    document.getElementById('animation-mouse').classList.add('blastIn');
-}
-
-document.getElementById('fadeOut').onclick = function () {
-    clearPreviousAnimation();
-    document.getElementById('animation-mouse').classList.add('fadeOut');
+    document.getElementById('target').classList.add('spin');
 }
 
 document.getElementById('fadeIn').onclick = function () {
     clearPreviousAnimation();
-    document.getElementById('animation-mouse').classList.add('fadeIn');
+    document.getElementById('target').classList.add('fadeIn');
+}
+
+document.getElementById('fadeOut').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('fadeOut');
+}
+
+document.getElementById('blastIn').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('blastIn');
+}
+
+document.getElementById('blastOut').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('blastOut');
+}
+
+document.getElementById('slightSRL').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('slightSRL');
+}
+
+document.getElementById('slightSLR').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('slightSLR');
+}
+
+document.getElementById('outSRL').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('outSRL');
+}
+
+document.getElementById('outSLR').onclick = function () {
+    clearPreviousAnimation();
+    document.getElementById('target').classList.add('outSLR');
 }
 
 function clearPreviousAnimation() {
-    document.getElementById('animation-mouse').className = "";
-    document.getElementById('animation-mouse').classList.add('saenimate');
-    document.getElementById('animation-mouse').classList.add('repeat-1');
-    document.getElementById('animation-mouse').classList.add('duration-1');
+    document.getElementById('target').className = "";
+    document.getElementById('target').classList.add('saenimate');
+    document.getElementById('target').classList.add('repeat-1');
+    document.getElementById('target').classList.add('duration-2');
 }
